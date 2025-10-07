d1d2c57e125663a1ff90647523d4eebb992dde4d
Elias Cooper <eqper228@gmail.com>
2025-10-07 14:55:09 +0300

    fix: add success

--- app/modules/admin/views/_partials/_alerts.php
+++ app/modules/admin/views/_partials/_alerts.php
@@ -6,6 +6,10 @@ if (Yii::$app->session->hasFlash('recordCreated')) {
 if (Yii::$app->session->hasFlash('recordUpdated')) {
     echo '<div class="alert alert-success text-left">The record was updated successfully</div>';
 }
+if (Yii::$app->session->hasFlash('success')) {
+    echo '<div class="alert alert-success text-left">'.Yii::$app->session->getFlash('success').'</div>';
+}
+
 if (Yii::$app->session->hasFlash('error')) {
     echo '<div class="alert alert-danger text-left">'.Yii::$app->session->getFlash('error').'</div>';
 }
